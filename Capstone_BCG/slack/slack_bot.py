f251c3935e3614fb59b9b3bb804f92f04e722501
karanmurthy7 <karanmurthy7.applications@gmail.com>
2018-04-02 22:52:21 -0700

    Updated slack_bot.py

--- Capstone_BCG/slack/slack_bot.py
+++ Capstone_BCG/slack/slack_bot.py
@@ -1,58 +1,62 @@
 from slackclient import SlackClient
 import time
+from boto.mws.connection import api_call_map
 
 
-class slackCommunication(object):
-	def __init__(self):
-		self.slack_client = SlackClient('')
-		self.appName = "nlpbot_test"
+class SlackCommunication(object):
+    def __init__(self): 
+        self.slack_client = SlackClient("xoxb-322946021959-ZulMf9FPaPZzoDeoph9RMdxo")
+        self.appName = 'test-bot'
+    
+    def slackConnection(self):
+        return self.slack_client.rtm_connect()
+    
+    def slackReadRTM(self):
+        return self.slack_client.rtm_read()
+    
+    
+    def parseSlackInput(self, input, botID):
+        botAtID = '<@' + botID + '>'
+        if input and len(input) > 0:
+            input = input[0]
+            if 'text' in input and botAtID in input['text']:
+                user = input['user']
+                message = input['text'][input['text'].index(' '):].strip()
+                channel = input['channel']
+                return [str(user), str(message), str(channel)]
+            else:
+                return [None, None, None]
+            
+    def getBotID(self, botUserName):
+        users_list = self.slack_client.api_call('users.list')
+        if (users_list):
+            users = users_list['members']
+            for user in users:
+                if ('name' in user and botUserName == user['name'] and not user.get('deleted')):
+                    return user.get('id')
+                
+    def writeToSlack(self, channel, message):
+        return self.slack_client.api_call('chat.postMessage', channel=channel, text=message, as_user=True)
+    
 
-	def slackConnect(self):	
-		return self.slack_client.rtm_connect()
-
-	def slackReadRTM(self):
-		return self.slack_client.rtm_read()
-
-	def parseSlackInput(self, input, botID):
-		botATID = botID
-		if input and len(input) > 0:
-			input = input[0]
-			if 'text' in input and botATID in input['text']:
-				user = input['user']
-				message = input['text']
-				channel = input['channel']
-				return [str(user), str(message), str(channel)]
-			else:
-				return [None, None, None]
-
-	def getBotID(self, botusername):
-		api_call = self.slack_client.api_call('users.list')
-		users = api_call['members']
-		for user in users:
-			if botusername in user.get('name') and not user.get('deleted'):
-				return user.get('id')
-
-	def writeToSlack(self, channel, message):
-		self.slack_client.api_call('chat.postMessage', channel = channel, text = "Chatbot")
-
-
-class mainFunc(slackCommunication):
-	def __init__(self):
-		super().__init__()
-
-	def decideWhethertotakeAction(self, input):
-		if input:
-			user, message, channel = input
-			return self.writeToSlack(channel, message)
-	
-	def run(self):
-		self.slackConnect()
-		BOTID = self.getBotID(self.appName)
-		while True:
-			self.decideWhethertotakeAction(self.parseSlackInput(self.slackReadRTM(), BOTID))
-			time.sleep(1)
-
+class MainFunction(SlackCommunication):
+    def __init__(self):
+        super(MainFunction, self).__init__()
+        
+    def decideWhetherToTakeAction(self, input):
+        if input:
+            user, message, channel = input
+            return self.writeToSlack(channel, message)
+        
+    def run(self):
+        self.slackConnection()
+        botID = self.getBotID(self.appName)
+        while True:
+            input = self.slackReadRTM()
+            self.decideWhetherToTakeAction(self.parseSlackInput(input, botID))
+            time.sleep(1)
+            
 
 if __name__ == '__main__':
-	instance = mainFunc()
-	instance.run()
+    instance = MainFunction()
+    instance.run()
